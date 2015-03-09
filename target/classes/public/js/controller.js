2be0694c84d1232a96d5035173d770555bd02538
Liqiang Cheng <liqiang.cheng@medtronic.com>
2015-03-08 23:23:30 -0500

    remove target folder

--- target/classes/public/js/controller.js
+++
@@ -1,7 +0,0 @@
-springBoot.controller('greetingController', function ($scope, $http) {
-    var url = "/greeting";
-    $http.get(url).success(function(data) {
-        console.log(data);
-        $scope.greeting = data;
-    });
-});
